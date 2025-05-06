18cb65932933b4c184f570d80856eaa582e594ce
rubenmg04 <133922854+rubenmg04@users.noreply.github.com>
2025-05-06 17:39:15 +0000

    Columna de nombre otra vez añadida

--- models.py
+++ models.py
@@ -1,7 +1,6 @@
 from sqlalchemy import Column, DateTime, Integer, String
 from app import db
 
-
 class Imagen(db.Model):
     __tablename__ = 'imagen'
     id = Column(Integer, primary_key=True)
@@ -11,6 +10,8 @@ class Imagen(db.Model):
     verde = Column(Integer, nullable=False)
     azul = Column(Integer, nullable=False)
     total = Column(Integer, nullable=False)
+    usuario = Column(String(100), nullable=False)  # <- NUEVO
 
     def __str__(self):
         return f"{self.nombre} ({self.fecha})"
+
